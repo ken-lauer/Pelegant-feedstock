ba2866ae3012e4df248ce3521a9cb1ad1d63e8d9
Ken Lauer <152229072+ken-lauer@users.noreply.github.com>
2023-12-21 14:27:55 -0600

    FIX: make sure we have write permissions to adjust rpath

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-set -xe
+set -ex -o pipefail
 
 mkdir oag
 mkdir epics
@@ -15,61 +15,6 @@ cp -a src/epics-base/* epics/
 cp -a src/epics-extensions/* epics/
 
 rm -rf src/
-
-PELEGANT_VERSION="${PELEGANT_VERSION=2023.3.0}"
-SDDS_VERSION="${SDDS_VERSION=5.5}"
-
-[ -z "$PELEGANT_VERSION" ] && { echo "PELEGANT_VERSION unset"; exit 1; }
-[ -z "$SDDS_VERSION" ] && { echo "SDDS_VERSION unset"; exit 1; }
-
-ELEGANT_ROOT="${SRC_DIR}/oag/apps/src/elegant/"
-
-echo "* Attempt at a basic Pelegant build tool"
-
-if ! command -v mpicc; then
-  echo "* mpicc not found? Was the environment built correctly?"
-  exit 1
-fi
-
-echo "* Version:      $PELEGANT_VERSION"
-echo "* SDDS:         $SDDS_VERSION"
-echo "* Work root:    $SRC_DIR"
-echo "* Elegant root: $ELEGANT_ROOT"
-echo "* Conda prefix: $PREFIX"
-
-set -ex -o pipefail
-
-echo "* Patching EPICS_BASE path for oag"
-# shellcheck disable=SC2016
-sed -i -e 's@^#\s*EPICS_BASE.*@EPICS_BASE=$(TOP)/../../epics/base@' "${SRC_DIR}/oag/apps/configure/RELEASE"
-
-if [[ $(uname -m) == 'arm64' ]]; then
-  echo "* Patching libpng config.h for ARM support"
-  # Ensure ARM support is configured or the build will fail
-  if grep 'undef PNG_ARM_NEON' epics/extensions/src/SDDS/png/config.h; then
-    patch --forward -p1 < "${RECIPE_DIR}/png_config.h.patch"
-  fi
-  echo "* Patching mpicc and mpicxx for recent macOS Xcode compatibility"
-  # See https://github.com/orgs/Homebrew/discussions/4797
-  # This is also a reason why we want our own conda build env...
-  sed -i '' \
-    's/final_ldflags="\(.*\),-commons,use_dylibs"/final_ldflags="\1"/' \
-    "$(readlink -f "$(which mpicc)")" \
-    "$(readlink -f "$(which mpicxx)")"
-else
-  echo "* ARM not detected; skipping libpng patch"
-fi
-
-echo "SDKROOT was $SDKROOT but we're unsetting it..."
-unset SDKROOT
-
-echo "* Setting up EPICS build system"
-pushd "${SRC_DIR}/epics/base" || exit
-make
-popd
-
-EPICS_HOST_ARCH=$("${SRC_DIR}"/epics/base/startup/EpicsHostArch)
-echo "* EPICS_HOST_ARCH=${EPICS_HOST_ARCH}"
 
 MAKE_ALL_ARGS=(
   "HDF_LIB_LOCATION=$PREFIX/lib" 
@@ -103,9 +48,47 @@ MPI_ARGS=(
 echo "* Make args:     ${MAKE_ALL_ARGS[@]}"
 echo "* Make MPI args: ${MAKE_MPI_ARGS[@]}"
 
-NUM_PROC=${NUM_PROC=8}
+if ! command -v mpicc; then
+  echo "* mpicc not found? Was the environment built correctly?"
+  exit 1
+fi
 
-echo "* Building in parallel with: ${NUM_PROC} processes"
+echo "* Work root:    $SRC_DIR"
+echo "* Conda prefix: $PREFIX"
+
+echo "* Patching EPICS_BASE path for oag"
+# shellcheck disable=SC2016
+sed -i -e 's@^#\s*EPICS_BASE.*@EPICS_BASE=$(TOP)/../../epics/base@' "${SRC_DIR}/oag/apps/configure/RELEASE"
+
+if [[ $(uname -m) == 'arm64' ]]; then
+  echo "* Patching libpng config.h for ARM support"
+  # Ensure ARM support is configured or the build will fail
+  if grep 'undef PNG_ARM_NEON' epics/extensions/src/SDDS/png/config.h; then
+    patch --forward -p1 < "${RECIPE_DIR}/png_config.h.patch"
+  fi
+  echo "* Patching mpicc and mpicxx for recent macOS Xcode compatibility"
+  # See https://github.com/orgs/Homebrew/discussions/4797
+  # This is also a reason why we want our own conda build env...
+  sed -i '' \
+    's/final_ldflags="\(.*\),-commons,use_dylibs"/final_ldflags="\1"/' \
+    "$(readlink -f "$(which mpicc)")" \
+    "$(readlink -f "$(which mpicxx)")"
+else
+  echo "* ARM not detected; skipping libpng patch"
+fi
+
+if [ -n "$SDKROOT" ]; then
+  echo "SDKROOT was $SDKROOT but we're unsetting it. MacOS builds will fail otherwise."
+  unset SDKROOT
+fi
+
+echo "* Setting up EPICS build system"
+pushd "${SRC_DIR}/epics/base" || exit
+make
+popd
+
+EPICS_HOST_ARCH=$("${SRC_DIR}"/epics/base/startup/EpicsHostArch)
+echo "* EPICS_HOST_ARCH=${EPICS_HOST_ARCH}"
 
 echo "* Patching SDDS utils"
 # APS may have this patched locally; these were changed long before 1.12.1
@@ -155,29 +138,30 @@ pushd "${SRC_DIR}/epics/extensions/src/SDDS/python" || exit
 make "${MPI_ARGS[@]}" PYTHON=310 PYTHON3=1
 popd
 
-echo "* Building Pelegant"
-pushd "${ELEGANT_ROOT}" || exit
 if command -v nlpp; then
   echo "* nlpp already in PATH: $(which nlpp)"
 else
   echo "* Adding extension bin directory to PATH for nlpp"
   export PATH="${SRC_DIR}/epics/extensions/bin/${EPICS_HOST_ARCH}:$PATH" 
 fi
+
+echo "* Building Pelegant"
+pushd "${SRC_DIR}/oag/apps/src/elegant/" || exit
 make Pelegant \
   "${MPI_ARGS[@]}" \
   GSL=1 \
   gsl_DIR="$PREFIX/lib" \
   gslcblas_DIR="$PREFIX/lib" \
-  USER_MPI_FLAGS="-DUSE_MPI=1 -DSDDS_MPI_IO=1 -I$PREFIX/include" \
-   || { echo waiting; sleep 1000000000; exit; }
+  USER_MPI_FLAGS="-DUSE_MPI=1 -DSDDS_MPI_IO=1 -I$PREFIX/include"
 popd
 
 PELEGANT_BINARY="${SRC_DIR}/oag/apps/bin/${EPICS_HOST_ARCH}/Pelegant"
 echo "* Done"
 
 echo "* Adjusting rpath for $PREFIX/lib"
-for fn in "${SRC_DIR}/oag/apps/bin/${EPICS_HOST_ARCH}/"* "${SRC_DIR}/epics/extensions/bin/${EPICS_HOST_ARCH}/"*; do
-  "${INSTALL_NAME_TOOL}" -add_rpath "$PREFIX/lib" "$fn"
+for binary in "${SRC_DIR}/oag/apps/bin/${EPICS_HOST_ARCH}/"* "${SRC_DIR}/epics/extensions/bin/${EPICS_HOST_ARCH}/"*; do
+  chmod +w "$binary"
+  "${INSTALL_NAME_TOOL}" -add_rpath "$PREFIX/lib" "$binary"
 done
 
 echo "* Trying out the Pelegant binary..."
